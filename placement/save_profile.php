968baa04e529ee832fda6826af9a7dbfe60cfd69
Aishwaryacse <119986852+Aishwaryacse@users.noreply.github.com>
2023-10-30 14:15:25 +0530

    Add files via upload

---
+++ placement/save_profile.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Save Profile</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-image: url('pic1.jpeg'); /* Background image */
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+        .container {
+            background-color: rgba(255, 255, 255, 0.7); /* Semi-transparent white background */
+            width: 80%;
+            max-width: 600px;
+            margin: 20px auto;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+        }
+        h1 {
+            text-align: center;
+        }
+        p {
+            text-align: center;
+            font-size: 18px;
+        }
+        .saved-data {
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin-top: 20px;
+            border-radius: 5px;
+        }
+        .edit-button {
+            text-align: center;
+        }
+        .edit-button a {
+            text-decoration: none;
+            background-color: #0077B5;
+            color: #fff;
+            padding: 10px 20px;
+            border-radius: 3px;
+        }
+        .edit-button a:hover {
+            background-color: #00568c;
+        }
+        .resume-link {
+            display: block;
+            margin-top: 10px;
+            text-decoration: underline;
+            color: #0077B5;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Profile Data Saved</h1>
+        <p>Your profile data has been successfully saved:</p>
+        <div class="saved-data">
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            // Retrieve data from the form
+            $name = $_POST["name"];
+            $registernumber = $_POST["registernumber"];
+            $gender = $_POST["gender"];
+            $dob = $_POST["dob"];
+            $department = $_POST["department"];
+            $yearofstudy = $_POST["yearofstudy"];
+            $tenthpercentage = $_POST["tenthpercentage"];
+            $twelfthpercentage = $_POST["twelfthpercentage"];
+            $ugpercentage = $_POST["ugpercentage"];
+            
+            // Check if a PDF file was uploaded
+            if (isset($_FILES["resume"]) && $_FILES["resume"]["error"] === UPLOAD_ERR_OK) {
+                $resumeFileName = $_FILES["resume"]["name"];
+                $resumeTmpName = $_FILES["resume"]["tmp_name"];
+                
+                // Move the uploaded PDF file to a destination directory
+                $uploadDirectory = "uploads/";
+                move_uploaded_file($resumeTmpName, $uploadDirectory . $resumeFileName);
+            } else {
+                $resumeFileName = ""; // No file was uploaded
+            }
+
+            // Display the saved profile data
+            echo "Name: $name<br>";
+            echo "Register Number: $registernumber<br>";
+            echo "Gender: $gender<br>";
+            echo "Date of Birth: $dob<br>";
+            echo "Department: $department<br>";
+            echo "Year of Study: $yearofstudy<br>";
+            echo "10th Percentage: $tenthpercentage<br>";
+            echo "12th Percentage: $twelfthpercentage<br>";
+            echo "UG Percentage: $ugpercentage<br>";
+            
+            // Display the resume as a clickable link
+            if (!empty($resumeFileName)) {
+                echo '<a class="resume-link" href="' . $uploadDirectory . $resumeFileName . '" target="_blank">View Resume</a>';
+            }
+
+            // Add an "Edit" button to navigate back to myprofile.php
+            echo '<div class="edit-button"><a href="myprofile.php">Edit Profile</a></div>';
+        } else {
+            echo "Invalid request.";
+        }
+        ?>
+        </div>
+    </div>
+</body>
+</html>
